6889bf56dbe6ac7a56fe2d6290642712515717d3
Dejan Tomic <hi@iamdejan.com>
2026-07-10 15:06:36 +0200

    platform/back: remove permissions addon

--- platform/back/addons/system/permissions/expose.js
+++
@@ -1,9 +0,0 @@
-import permissions from '#shared/system/permissions/addon.js';
-
-permissions.Expose({
-	filter: ['id', 'name', 'application_id'],
-	sort: ['name', 'created_at', 'updated_at'],
-	select: [
-		'id', 'name', 'label', 'description', 'application_id', 'updated_at', 'created_at'
-	]
-});
