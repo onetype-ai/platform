0382dd3afc467107064bbd0e32f5d2a15ebaea9b
Dejan Tomic <hi@iamdejan.com>
2026-07-10 15:06:36 +0200

    platform/back: remove fonts addon

--- platform/back/addons/fonts/expose.js
+++
@@ -1,9 +0,0 @@
-import fonts from '#fonts/addon.js';
-
-fonts.Expose({
-	filter: ['id', 'name', 'category', 'popular'],
-	sort: ['name', 'category', 'updated_at', 'created_at'],
-	select: [
-		'id', 'name', 'slug', 'category', 'popular', 'weights', 'styles', 'updated_at', 'created_at'
-	]
-});
