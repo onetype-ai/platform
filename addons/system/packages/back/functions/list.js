0e8d2659df7c2b689cbaca65212914a1217f552a
Dejan Tomic <hi@iamdejan.com>
2026-07-11 21:28:59 +0200

    packages: state carries limits, features, permissions and config

--- addons/system/packages/back/functions/list.js
+++ addons/system/packages/back/functions/list.js
@@ -4,6 +4,9 @@ packages.Fn('list', function()
 {
 	return Object.values(this.Items()).map((item) =>
 	{
-		return item.Get(['slug', 'type', 'name', 'version', 'description', 'icon', 'color', 'status']);
+		return {
+			...item.Get(['slug', 'type', 'name', 'version', 'description', 'icon', 'color', 'status', 'permissions', 'features', 'config']),
+			limits: this.Fn('limits', item.Get('slug'))
+		};
 	});
 });
